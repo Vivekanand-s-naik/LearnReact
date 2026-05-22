063f9c0fb3a71bb172586c1c7c427c64f0ec3a8c
Vivekanand-s-naik <shreeveeraganapati15@gmail.com>
2026-05-22 18:49:25 +0530

    day-y Learnt theme control gradient animation transition etc

--- LearnTailWind/src/compoments/Card.jsx
+++ LearnTailWind/src/compoments/Card.jsx
@@ -1,20 +1,20 @@
 function Card({img, role}) {
   return (
-    <div className="relative w-90 h-90 group mx-auto ">
+    <div className="relative w-70 h-80 group mx-auto bg-black rounded-2xl overflow-hidden shadow-[0px_0px_50px_#FFE786] dark:shadow-[0px_0px_50px_#101044] text-white">
       <img
         src={img}
-        className="absolute inset-0 h-full w-full object-cover opacity-90 transition-opacity duration-100 group-hover:opacity-40 rounded-2xl"
+        className="absolute inset-0 h-full w-full object-cover opacity-90 transition-opacity duration-100 group-hover:opacity-40 "
       />
       <div className="p-4 relative flex flex-col gap-y-40">
         <div className="">
           <p className="text-[20px] tracking-wider text-[#F6339A] font-bold">
             {role}
           </p>
-          <p className="text-3xl tracking-wider font-bold">Vivekanand</p>
+          <p className="text-3xl tracking-wider font-bold text-white">Vivekanand</p>
         </div>
 
         <div className="">
-          <p className="text-sm translate-y-4 opacity-0 transition-transform group-hover:opacity-100">
+          <p className="opacity-0 transition-transform translate-y-8 group-hover:translate-y-0 group-hover:opacity-100">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
             perferendis hic asperiores quibusdam quidem voluptates doloremque
             reiciendis nostrum harum. Repudiandae?
