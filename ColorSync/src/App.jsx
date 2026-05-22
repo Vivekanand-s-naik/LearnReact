383d15c8bb153d262f55f341cf2a51073cff6276
Vivekanand-s-naik <shreeveeraganapati15@gmail.com>
2026-05-23 01:26:20 +0530

    spinning-animation

--- ColorSync/src/App.jsx
+++ ColorSync/src/App.jsx
@@ -20,98 +20,105 @@ function App() {
     <>
       {" "}
       <main>
-        <div className={`${color} h-screen w-screen relative flex justify-center`}>
-          <div className="absolute bottom-12  bg-white h-auto w-[93%] p-3.5 rounded-4xl">
-            <ul className="[&>li>button]:h-16 [&>li>button]:w-28 [&>li>button]:rounded-4xl flex flex-wrap justify-center gap-4">
-              <li>
-                <button
-                  className="bg-[red]"
-                  onClick={() => setColor(colors["red"])}
-                >
-                  Red
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[green]"
-                  onClick={() => setColor(colors["green"])}
-                >
-                  Green
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[blue]"
-                  onClick={() => setColor(colors["blue"])}
-                >
-                  Blue
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[#7E7C00]"
-                  onClick={() => setColor(colors["olive"])}
-                >
-                  Olive
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[#7E7E80]"
-                  onClick={() => setColor(colors["grey"])}
-                >
-                  Grey
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[yellow]"
-                  onClick={() => setColor(colors["yellow"])}
-                >
-                  Yellow
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[pink]"
-                  onClick={() => setColor(colors["pink"])}
-                >
-                  Pink
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[purple]"
-                  onClick={() => setColor(colors["purple"])}
-                >
-                  Purple
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[#E5E5FB]"
-                  onClick={() => setColor(colors["lavender"])}
-                >
-                  Lavender
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[white] text-black"
-                  onClick={() => setColor(colors["white"])}
-                >
-                  White
-                </button>
-              </li>
-              <li>
-                <button
-                  className="bg-[black] text-white"
-                  onClick={() => setColor(colors["black"])}
-                >
-                  Black
-                </button>
-              </li>
-            </ul>
+        <div
+          className={`transition-colors ${color} h-screen w-screen relative flex justify-center`}
+        >
+          <div className="absolute bottom-12 w-[90%] ">
+            <div className="relative overflow-hidden rounded-4xl lg:h-30 md:h-56 sm:h-64 flex items-center">
+              <div className="[animation-duration:2s] animate-spin absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-conic-180 from-cyan-400 via-fuchsia-500 to-blue-700 h-screen w-screen"></div>
+              <div className="relative left-1/2 -translate-x-1/2 z-10 bg-white w-[99.3%] lg:h-28 md:h-54 sm:h-60 flex items-center justify-center rounded-4xl ">
+                <ul className="[&>li>button]:h-16 [&>li>button]:w-28 [&>li>button]:rounded-4xl flex flex-wrap justify-center gap-3">
+                  <li>
+                    <button
+                      className="bg-[red]"
+                      onClick={() => setColor(colors["red"])}
+                    >
+                      Red
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[green]"
+                      onClick={() => setColor(colors["green"])}
+                    >
+                      Green
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[blue]"
+                      onClick={() => setColor(colors["blue"])}
+                    >
+                      Blue
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[#7E7C00]"
+                      onClick={() => setColor(colors["olive"])}
+                    >
+                      Olive
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[#7E7E80]"
+                      onClick={() => setColor(colors["grey"])}
+                    >
+                      Grey
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[yellow]"
+                      onClick={() => setColor(colors["yellow"])}
+                    >
+                      Yellow
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[pink]"
+                      onClick={() => setColor(colors["pink"])}
+                    >
+                      Pink
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[purple]"
+                      onClick={() => setColor(colors["purple"])}
+                    >
+                      Purple
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[#E5E5FB]"
+                      onClick={() => setColor(colors["lavender"])}
+                    >
+                      Lavender
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[white] text-black"
+                      onClick={() => setColor(colors["white"])}
+                    >
+                      White
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bg-[black] text-white"
+                      onClick={() => setColor(colors["black"])}
+                    >
+                      Black
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </main>
