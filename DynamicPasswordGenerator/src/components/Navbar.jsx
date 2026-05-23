5e80fc7f167d87a66c60f410389f41a10111cf46
Vivekanand-s-naik <shreeveeraganapati15@gmail.com>
2026-05-23 20:09:58 +0530

    Day-6

---
+++ DynamicPasswordGenerator/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+function Navbar({ item, setItem }) {
+  console.log("Child Element Renders");
+  return (
+    <div>
+      Navbar{item}
+      <button onClick={()=>setItem()}>Click Me</button>
+    </div>
+  );
+}
+
+export default memo(Navbar);
